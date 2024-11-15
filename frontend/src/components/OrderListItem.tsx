df053ffa20906c7c456e47b42431678ad1e48c67
sorongosdev <dosora9@gmail.com>
2024-11-15 20:46:54 +0900

    feat: 주문내역 api 연동 전 테스트 코드 추가

--- frontend/src/components/OrderListItem.tsx
+++ frontend/src/components/OrderListItem.tsx
@@ -1,10 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {TouchableOpacity, Text, View} from 'react-native';
+import axios from 'axios';
+
 /** Icons */
 import DetailIcon from '@assets/icon_details.svg';
 import EmptyLikeIcon from '@assets/icon_empty_like.svg';
 import FullLikeIcon from '@assets/icon_full_like.svg';
 import CloseIcon from '@assets/icon_cancel.svg';
+/** Components */
+import {getToken} from '../components/UserToken';
+/** Const */
+import {BASE_URL} from '../consts/Url';
 /** Styles */
 import styles from '../styles/OrderListItem';
 /** Props */
@@ -30,9 +36,46 @@ export default function OrderListItem({
 }: CombinedInterface): React.JSX.Element {
   const [likeChecked, setLikeChecked] = useState<boolean>(false);
 
+  const postStoreLike = async (token: string) => {
+    try {
+      const response = await axios.post(
+        `${BASE_URL}/user/wish`,
+        {
+          token,
+          type: 'store',
+          store_id: 1002,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      console.log('like post data', response.data);
+    } catch (error) {
+      console.error('Error posting like', error);
+    }
+  };
+
+  useEffect(() => {
+    const handleLike = async () => {
+      if (likeChecked) {
+        console.log('Like 버튼이 체크되었습니다.');
+        const userToken = await getToken(); // await 사용
+        console.log('token', userToken);
+        if (userToken) {
+          await postStoreLike(userToken); // await 사용
+        }
+      }
+    };
+
+    handleLike();
+  }, [likeChecked]); // likeChecked가 변경될 때마다 실행
+
   const navigateToPay = () => {
     navigation.navigate('Pay');
   };
+
   return (
     <View>
       <View style={styles.dateContainer}>
@@ -50,7 +93,7 @@ export default function OrderListItem({
             <View style={styles.storeWrapper}>
               <Text style={styles.storeText}>{storeName}</Text>
               <View style={styles.detailIconBox}>
-                <DetailIcon></DetailIcon>
+                <DetailIcon />
               </View>
             </View>
             <Text style={styles.menuText}>{menuName}</Text>
