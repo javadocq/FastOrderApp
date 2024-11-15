2a84202b71c74a0e6dc8995610abc43f4f4159fe
Sorongos <dosora9@gmail.com>
2024-11-15 12:16:18 -0800

    feat: 주문내역 불러오기, 찜 기능 구현
    편집 후 주문 내역 삭제
    전체 주문 내역 불러오기 해야함

--- frontend/src/components/OrderListItem.tsx
+++ frontend/src/components/OrderListItem.tsx
@@ -1,5 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {TouchableOpacity, Text, View} from 'react-native';
+import axios from 'axios';
+import {getToken} from '../components/UserToken';
+/** Consts */
+import {BASE_URL} from '../consts/Url';
+/** Components */
+import MainModal from './MainModal';
+
 /** Icons */
 import DetailIcon from '@assets/icon_details.svg';
 import EmptyLikeIcon from '@assets/icon_empty_like.svg';
@@ -11,14 +18,21 @@ import styles from '../styles/OrderListItem';
 import {NavigationProp} from '../navigation/NavigationProps';
 
 interface OrderListProp {
+  editButtonClicked: boolean;
+}
+
+interface StoreProp {
   date: string;
   progress: string;
   storeName: string;
   menuName: string;
-  editButtonClicked: boolean;
+  store_logo: string;
+  is_wished: boolean;
+  storeId: number;
+  updateWishStatus: (storeId: number, newStatus: boolean) => void; // 상태 업데이트 함수 타입 정의
 }
 
-interface CombinedInterface extends NavigationProp, OrderListProp {}
+interface CombinedInterface extends NavigationProp, OrderListProp, StoreProp {}
 
 export default function OrderListItem({
   navigation,
@@ -27,12 +41,61 @@ export default function OrderListItem({
   storeName,
   menuName,
   editButtonClicked,
+  is_wished,
+  storeId,
+  updateWishStatus,
 }: CombinedInterface): React.JSX.Element {
   const [likeChecked, setLikeChecked] = useState<boolean>(false);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLikeChecked(is_wished);
+  }, [is_wished]);
 
   const navigateToPay = () => {
     navigation.navigate('Pay');
   };
+
+  const navigateToStore = () => {
+    console.log('store id ', storeId);
+    navigation.navigate('Store', {storeId});
+  };
+
+  const handleLikePress = () => {
+    if (likeChecked) {
+      setModalVisible(true);
+    } else {
+      setLikeChecked(true); // 상태를 true로 변경
+      updateWishStatus(storeId, true); // 상태 업데이트 호출
+      postLikes(); // 서버에 좋아요 추가 요청
+    }
+  };
+
+  const confirmLike = () => {
+    setLikeChecked(false); // 상태를 false로 변경
+    updateWishStatus(storeId, false); // 상태 업데이트 호출
+    setModalVisible(false);
+  };
+
+  const cancelLike = () => {
+    setModalVisible(false);
+    setLikeChecked(is_wished); // 초기 상태로 복원
+  };
+
+  const postLikes = async () => {
+    try {
+      const token = await getToken();
+      const response = await axios.post(`${BASE_URL}/user/wish`, {
+        token: token,
+        type: 'store',
+        store_id: storeId,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log('Error during Main List Like', error);
+    }
+  };
+
   return (
     <View>
       <View style={styles.dateContainer}>
@@ -55,17 +118,11 @@ export default function OrderListItem({
             </View>
             <Text style={styles.menuText}>{menuName}</Text>
           </View>
-          <View style={styles.likeIconBox}>
+          <TouchableOpacity
+            style={styles.likeIconBox}
+            onPress={handleLikePress}>
             <View style={{display: editButtonClicked ? 'none' : 'flex'}}>
-              {likeChecked ? (
-                <TouchableOpacity onPress={() => setLikeChecked(false)}>
-                  <FullLikeIcon />
-                </TouchableOpacity>
-              ) : (
-                <TouchableOpacity onPress={() => setLikeChecked(true)}>
-                  <EmptyLikeIcon />
-                </TouchableOpacity>
-              )}
+              {likeChecked ? <FullLikeIcon /> : <EmptyLikeIcon />}
             </View>
             <CloseIcon
               style={[
@@ -73,12 +130,18 @@ export default function OrderListItem({
                 {display: editButtonClicked ? 'flex' : 'none'},
               ]}
             />
-          </View>
+          </TouchableOpacity>
         </View>
       </View>
       <TouchableOpacity style={styles.orderButton} onPress={navigateToPay}>
         <Text style={styles.orderText}>같은 메뉴 주문하기</Text>
       </TouchableOpacity>
+
+      <MainModal
+        visible={modalVisible}
+        onClose={cancelLike}
+        onConfirm={confirmLike}
+      />
     </View>
   );
 }
--- frontend/src/pages/Order.tsx
+++ frontend/src/pages/Order.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import {NavigationProp} from '../navigation/NavigationProps';
+import axios from 'axios';
 
 /** Style */
 import styles from '../styles/Order';
@@ -8,51 +9,57 @@ import styles from '../styles/Order';
 import OrderListItem from '../components/OrderListItem';
 import AppbarSmall from '../components/AppbarSmall';
 import {ScrollView} from 'react-native-gesture-handler';
+import {getToken} from '../components/UserToken';
+/** Consts */
+import {BASE_URL} from '../consts/Url';
+
+interface Store {
+  order_date: string;
+  order_status: string;
+  is_wished: boolean;
+  store_name: string;
+  store_logo: string;
+  menus: Menu[];
+  store_id: number;
+}
+
+interface Menu {
+  menu_name: string;
+}
 
 export default function Order({navigation}: NavigationProp): React.JSX.Element {
-  const [recentMenu, setRecentMenu] = useState([
-    {
-      date: '8.17(수)',
-      progress: '픽업완료',
-      storeName: '찌개찌개',
-      menuName: '김치찌개 외 1개 28,000원',
-    },
-    {
-      date: '8.16(화)',
-      progress: '픽업완료',
-      storeName: '찌개찌개',
-      menuName: '된장찌개 외 1개 27,000원',
-    },
-    {
-      date: '8.15(월)',
-      progress: '픽업완료',
-      storeName: '찌개찌개',
-      menuName: '김치찌개 외 1개 26,000원',
-    },
-    {
-      date: '8.17(수)',
-      progress: '픽업완료',
-      storeName: '찌개찌개',
-      menuName: '김치찌개 외 1개 28,000원',
-    },
-    {
-      date: '8.16(화)',
-      progress: '픽업완료',
-      storeName: '찌개찌개',
-      menuName: '된장찌개 외 1개 27,000원',
-    },
-    {
-      date: '8.15(월)',
-      progress: '픽업완료',
-      storeName: '찌개찌개',
-      menuName: '김치찌개 외 1개 26,000원',
-    },
-  ]);
+  const [historys, setHistorys] = useState<Store[]>([]);
   const [editButton, setEditButton] = useState(false);
+
+  useEffect(() => {
+    const getSearchResult = async () => {
+      try {
+        const token = await getToken();
+        const response = await axios.get(
+          `${BASE_URL}/orders/history?token=${token}`,
+        );
+
+        setHistorys(response.data.order_history);
+      } catch (e) {
+        console.log('Search Result Error: ', e);
+      }
+    };
+    getSearchResult();
+  }, []);
 
   const toggleEditButton = () => {
     setEditButton(prevEditButton => !prevEditButton);
     console.log('edit button pressed');
+  };
+
+  const updateWishStatus = (storeId: number, newStatus: boolean) => {
+    setHistorys(prevHistory =>
+      prevHistory.map(history =>
+        history.store_id === storeId
+          ? {...history, is_wished: newStatus}
+          : history,
+      ),
+    );
   };
 
   return (
@@ -69,16 +76,27 @@ export default function Order({navigation}: NavigationProp): React.JSX.Element {
       <ScrollView
         style={styles.orderListContainer}
         contentContainerStyle={{paddingVertical: 10}}>
-        {recentMenu.map((menu, index) => (
-          <OrderListItem
-            navigation={navigation}
-            date={menu.date}
-            progress={menu.progress}
-            storeName={menu.storeName}
-            menuName={menu.menuName}
-            editButtonClicked={editButton}
-          />
-        ))}
+        {historys.map((history, index) => {
+          const menuCount = history.menus.length;
+          const menuName =
+            menuCount === 1
+              ? history.menus[0].menu_name
+              : `${history.menus[0].menu_name} 외 ${menuCount - 1}개`;
+          return (
+            <OrderListItem
+              navigation={navigation}
+              date={history.order_date}
+              progress={history.order_status}
+              storeName={history.store_name}
+              menuName={menuName.toString()}
+              editButtonClicked={editButton}
+              updateWishStatus={updateWishStatus}
+              store_logo={history.store_logo}
+              is_wished={history.is_wished}
+              storeId={history.store_id}
+            />
+          );
+        })}
       </ScrollView>
     </View>
   );
