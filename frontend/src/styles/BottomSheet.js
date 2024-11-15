7e8573a7ad3e1109883b87b50ff37c7ca1565fac
Sorongos <dosora9@gmail.com>
2024-11-15 08:30:07 -0800

    style: 바텀시트 연한 보더

--- frontend/src/styles/BottomSheet.js
+++ frontend/src/styles/BottomSheet.js
@@ -2,32 +2,25 @@ import {StyleSheet} from 'react-native';
 import {Dimensions} from 'react-native';
 import {HOME} from '../consts/BottomSheetConsts';
 
-const SEARCHBAR_HEIGHT = HOME.SEARCHBAR_HEIGHT;
-const {height} = Dimensions.get('window');
-
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
     justifyContent: 'flex-end',
     width: '100%',
-    // height: height,
     position: 'relative',
-    // borderWidth: 1,
   },
   sheet: {
-    // height: height - SEARCHBAR_HEIGHT,
     backgroundColor: 'white',
     borderRadius: 24,
     borderBottomEndRadius: 0,
     borderBottomStartRadius: 0,
     position: 'absolute',
     bottom: 0,
-    // height: '100%',
     width: '100%',
     display: 'flex',
     flexDirection: 'column',
     paddingHorizontal: 23,
-    // borderWidth: 1,
+    borderWidth: 1,
+    borderColor: 'rgba(183, 183, 183, 0.50)',
   },
   handleBox: {
     display: 'flex',
