228a9b6f7b53d498c8f7b3f6b95ab5772c1a0cba
Sorongos <dosora9@gmail.com>
2024-11-15 09:46:38 -0800

    feat: Main 주문내역 불러오기
    store img, store 찜 해야함

--- frontend/src/components/OrderHistory.tsx
+++ frontend/src/components/OrderHistory.tsx
@@ -1,58 +1,93 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
+import axios from 'axios';
 
 import {NavigationProp} from '../navigation/NavigationProps';
-
+/** Consts */
+import {BASE_URL} from '../consts/Url';
 /** Components */
 import MainListItem from '../components/MainListItem';
+import {getToken} from '../components/UserToken';
+
 /** Styles */
 import styles from '../styles/OrderHistory';
+
+interface Store {
+  order_date: string;
+  order_status: string;
+  is_wished: boolean;
+  store_name: string;
+  store_logo: string;
+  menus: Menu[];
+}
+
+interface User {
+  user_name: string;
+}
+
+interface Menu {
+  menu_name: string;
+}
 
 export default function OrderHistory({
   navigation,
 }: NavigationProp): React.JSX.Element {
-  const [recentMenu, setRecentMenu] = useState([
-    {
-      date: '8.17(수)',
-      progress: '픽업완료',
-      like: false,
-      storeName: '찌개찌개',
-      menuName: '김치찌개 외 1개 28,000원',
-    },
-    {
-      date: '8.16(화)',
-      progress: '픽업완료',
-      like: false,
-      storeName: '찌개찌개',
-      menuName: '된장찌개 외 1개 27,000원',
-    },
-    {
-      date: '8.15(월)',
-      progress: '픽업완료',
-      like: false,
-      storeName: '찌개찌개',
-      menuName: '김치찌개 외 1개 26,000원',
-    },
-  ]);
+  const [historys, setHistorys] = useState<Store[]>([]);
+  const [userName, setUserName] = useState<string>();
+
+  useEffect(() => {
+    const getSearchResult = async () => {
+      try {
+        const token = await getToken();
+        console.log('token ', token);
+
+        const response = await axios.get(
+          `${BASE_URL}/orders/history?token=${token}`,
+        );
+
+        console.log('data  ', response.data);
+
+        setHistorys(response.data.order_history);
+        setUserName(response.data.user_name);
+        console.log('order history ', historys);
+        console.log('user name ', userName);
+      } catch (e) {
+        console.log('Search Result Error: ', e);
+      }
+    };
+    getSearchResult(); // 함수 호출
+  }, []);
 
   return (
     <View>
-      <Text style={styles.bottomSheetTitle}>ㅇㅇ님의 최근 주문내역이에요!</Text>
+      <Text style={styles.bottomSheetTitle}>
+        {userName}님의 최근 주문내역이에요!
+      </Text>
 
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{paddingBottom: 68}}>
-        {recentMenu.map((menu, index) => (
-          <MainListItem
-            key={index}
-            navigation={navigation}
-            date={menu.date}
-            progress={menu.progress}
-            storeName={menu.storeName}
-            menuName={menu.menuName}
-          />
-        ))}
+        {historys.map((history, index) => {
+          const menuCount = history.menus.length;
+          const menuName =
+            menuCount === 1
+              ? history.menus[0].menu_name
+              : `${history.menus[0].menu_name} 외 ${menuCount - 1}개`;
+
+          console.log('menuName ', menuName);
+
+          return (
+            <MainListItem
+              key={index}
+              navigation={navigation}
+              date={history.order_date}
+              progress={history.order_status}
+              storeName={history.store_name}
+              menuName={menuName.toString()}
+            />
+          );
+        })}
       </ScrollView>
     </View>
   );
