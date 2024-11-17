ff5d942a6d089cd2674365550b375ee73fbb742e
Sorongos <dosora9@gmail.com>
2024-11-17 06:50:12 -0800

    feat: 타입 오류 안 나는 버전

--- frontend/src/components/FCMHandler.tsx
+++ frontend/src/components/FCMHandler.tsx
@@ -2,36 +2,34 @@ import React, {useEffect} from 'react';
 import {Alert, Platform} from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 import FlashMessage, {showMessage} from 'react-native-flash-message';
-import PushNotificationIOS from '@react-native-community/push-notification-ios'; // iOS에서 사용하는 PushNotificationIOS
-
-let PushNotification; // 타입을 `any`로 설정 (임포트할 때 사용)
-
-if (Platform.OS === 'android') {
-  PushNotification = require('react-native-push-notification').default; // 수정된 부분
-}
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import PushNotification from 'react-native-push-notification';
 
 const FCMHandler: React.FC = () => {
   useEffect(() => {
     // PushNotification 설정 (Android에서만 사용)
-    if (PushNotification) {
-      PushNotification.configure({
-        onNotification: function (notification) {
-          console.log('NOTIFICATION:', notification);
-        },
-        requestPermissions: Platform.OS === 'ios',
-      });
-    }
+    PushNotification.configure({
+      onNotification: function (notification: any) {
+        // 타입을 any로 설정
+        console.log('NOTIFICATION:', notification);
+      },
+      requestPermissions: Platform.OS === 'ios',
+    });
 
     // iOS에서 PushNotificationIOS 사용
     if (Platform.OS === 'ios') {
-      PushNotificationIOS.addEventListener('notification', notification => {
-        console.log('Received notification in iOS:', notification);
-        Alert.alert(notification.getTitle(), notification.getBody());
-      });
+      PushNotificationIOS.addEventListener(
+        'notification',
+        (notification: any) => {
+          // 타입을 any로 설정
+          console.log('Received notification in iOS:', notification);
+          Alert.alert(notification.getTitle(), notification.getBody());
+        },
+      );
     }
 
     // 안드로이드에서만 알림 채널 생성
-    if (Platform.OS === 'android' && PushNotification) {
+    if (Platform.OS === 'android') {
       PushNotification.createChannel(
         {
           channelId: 'default_channel',
@@ -40,7 +38,8 @@ const FCMHandler: React.FC = () => {
           soundName: 'default',
           vibrate: true,
         },
-        created => console.log(`createChannel returned '${created}'`),
+        (created: boolean) =>
+          console.log(`createChannel returned '${created}'`),
       );
     }
 
@@ -51,7 +50,7 @@ const FCMHandler: React.FC = () => {
       const body = remoteMessage.notification?.body || 'Default Body';
 
       // 상태바에 알림 표시 (Android에서만)
-      if (Platform.OS === 'android' && PushNotification) {
+      if (Platform.OS === 'android') {
         PushNotification.localNotification({
           channelId: 'default_channel',
           title: title,
@@ -68,7 +67,7 @@ const FCMHandler: React.FC = () => {
       const body = remoteMessage.notification?.body || 'Default Body';
 
       // 상태바에 알림 표시 (Android에서만)
-      if (Platform.OS === 'android' && PushNotification) {
+      if (Platform.OS === 'android') {
         PushNotification.localNotification({
           channelId: 'default_channel',
           title: title,
